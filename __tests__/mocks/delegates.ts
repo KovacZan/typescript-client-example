cab341793eaf28960221848f6e20d3c2d5474162
KovacZan <zan@protokol.com>
2021-01-04 16:29:50 +0100

    Initial commit

---
+++ __tests__/mocks/delegates.ts
@@ -0,0 +1,24 @@
+import nock from "nock";
+
+export const mockDelegates = (host: string) => {
+	nock(host)
+		.get("/delegates")
+		.reply(200, { data: [] });
+
+	nock(host)
+		.get("/delegates/123")
+		.reply(200, { data: [] });
+
+	nock(host)
+		.get("/delegates/123/blocks")
+		.reply(200, { data: [] });
+
+	nock(host)
+		.get("/delegates/123/voters")
+		.reply(200, { data: [] });
+
+
+	nock(host)
+		.post("/delegates/search")
+		.reply(200, { data: [] });
+};
---
+++ src/resources/index.ts
@@ -0,0 +1,29 @@
+import { Blocks } from "./blocks";
+import { Delegates } from "./delegates";
+import { Entities } from "./entities";
+import { Locks } from "./locks";
+import { Node } from "./node";
+import { Peers } from "./peers";
+import { Rounds } from "./rounds";
+import { Transactions } from "./transactions";
+import { Votes } from "./votes";
+import { Wallets } from "./wallets";
+
+export * from "./resource";
+
+// tslint:disable-next-line: variable-name
+export const Resources = {
+	blocks: Blocks,
+	delegates: Delegates,
+	entities: Entities,
+	locks: Locks,
+	node: Node,
+	peers: Peers,
+	rounds: Rounds,
+	transactions: Transactions,
+	votes: Votes,
+	wallets: Wallets,
+};
+
+export type AvailableResourcesName = keyof typeof Resources;
+export type AvailableResource<T extends AvailableResourcesName> = InstanceType<typeof Resources[T]>;
